bcdd8797375425b65f30c51677abd7f839b8c8b6
Vinicius Lima <gvlima.contato@gmail.com>
2025-02-01 16:05:21 -0300

    :books: fazendo novas alterações, de forma dinamica com a biblioteca  AOS

---
+++ scripts/aos.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    AOS.init({
+        offset: 120, 
+        delay: 100, 
+        duration: 1000,
+        once: false,
+        mirror: true,
+        easing: 'ease-in-out',
+    });
+});
